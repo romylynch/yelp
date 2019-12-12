0b35255ddea5b03fd3c279e953dac7f47e4e11e7
Romy Lynch <romylynch@gmail.com>
2019-12-12 10:33:55 +0000

    Namespace admin and nested reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    collection do
+      get "top", to: "restaurants#top"
+    end
+    member do
+      get "chef", to: "restaurants#chef"
+    end
+    resources :reviews, only: [:new, :create]
+  
+  end
+  namespace :admin do
+     resources :restaurants
+  end
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    stars:        3,
+    chef:         "Jamie Oliver"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    stars:        2,
+    chef:          "Gordon Ramsey"
+  }, 
+  {
+    name:         'Le pré Catelan',
+    address:      'route de Suresnes 75016 Paris',
+    description:  'Oeil vif, geste sur: impossible de distinguer',
+    stars:        3,
+    chef:          "Alain Ducasse"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
